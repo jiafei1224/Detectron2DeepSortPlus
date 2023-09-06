69210bb55bf8593f9b1575eb7e21e5539422b4c0
Jiafei Duan <51585075+jiafei1224@users.noreply.github.com>
2023-09-05 20:25:18 -0700

    Update yl2ds.py

--- yl2ds.py
+++ yl2ds.py
@@ -9,14 +9,63 @@ from tqdm import tqdm
 from yolov5_dt import Yolov5Detector
 from sort import Sort
 from deep_sort import DeepSort
+import os  # Make sure you import os
 
 from util import draw_bboxes, draw_detections
 
+import cv2
+import os
+
+
+
+
 def main():
     args = get_parser().parse_args()
+
+    # Create output directory for frames if it doesn't exist
+    output_directory = "frames"
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    # Open the video file
+    cap = cv2.VideoCapture(args.input)
+
+    # Get video information
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+
+    # Set the desired FPS
+    desired_fps = 30
+
+    # Calculate skip frames based on original and desired FPS
+    skip_frames = max(1, int(fps / desired_fps))  # Ensure skip_frames is at least 1
+
+    # Initialize frame counter
+    frame_count = 0
+
+    while True:
+        ret, frame = cap.read()
+        
+        # Break the loop if the video has ended
+        if not ret:
+            break
+
+        # Save only every nth frame based on desired FPS
+        if frame_count % skip_frames == 0:
+            frame_number = frame_count // skip_frames
+            frame_filename = os.path.join(output_directory, f"{frame_number}.png")
+            cv2.imwrite(frame_filename, frame)
+            print(f"Saved {frame_filename}")
+
+        frame_count += 1
+
+    # Release video capture
+    cap.release()
+
+    print("Done!")
+
     if args.display:
         cv2.namedWindow("out_vid", cv2.WINDOW_NORMAL)
-        cv2.resizeWindow("out_vid", 960, 720)
+        cv2.resizeWindow("out_vid", 1080, 1920)
     detector = Yolov5Detector(args.weights, args.img_size, args.conf_thres, args.nms_max_overlap, args.device, args.agnostic_nms)
     sort = Sort()
     deepsort = DeepSort(args.deepsort_checkpoint, nms_max_overlap=args.nms_max_overlap, use_cuda=bool(strtobool(args.use_cuda)))
@@ -26,7 +75,7 @@ def main():
             filename=args.out_vid,
             fourcc=cv2.VideoWriter_fourcc(*'MJPG'),
             fps=1.0,
-            frameSize=(1920, 1440),
+            frameSize=(1080, 1920),
         )
     if args.out_txt:
         out_txt = open(args.out_txt, "w+")
@@ -67,8 +116,29 @@ def main():
             if args.out_txt:
                 for i in range(len(ordered_identities)):
                     tlbr = tlbr_boxes[i]
-                    line = [frameID+1, ordered_identities[i], tlbr[0], tlbr[1], tlbr[2]-tlbr[0], tlbr[3]-tlbr[1], 1, 1, 1]
+                    center_x = (tlbr[0] + tlbr[2]) / 2  # Center x-coordinate
+                    center_y = (tlbr[1] + tlbr[3]) / 2  # Center y-coordinate
+                    line = [frameID+1, center_x, center_y]
                     out_txt.write(",".join(str(item) for item in line) + "\n")
+                    
+                    # top_left = (tlbr[0], tlbr[1])
+                    # top_right = (tlbr[0], tlbr[3])
+                    # bottom_left = (tlbr[2], tlbr[1])
+                    # bottom_right = (tlbr[2], tlbr[3])
+                    
+                    # # Prepare the line to write in the output text file
+                    # line = [
+                    #     frameID+1, 
+                    #     ordered_identities[i],
+                    #     top_left[0], top_left[1],  # Top-left coordinates
+                    #     top_right[0], top_right[1],  # Top-right coordinates
+                    #     bottom_left[0], bottom_left[1],  # Bottom-left coordinates
+                    #     bottom_right[0], bottom_right[1]  # Bottom-right coordinates
+                    # ]
+                    
+                    # out_txt.write(",".join(str(item) for item in line) + "\n")
+
+
         end = time.time()
         im = cv2.putText(im, "Frame ID: "+str(frameID+1), (20,30), 0, 5e-3 * 200, (0,255,0), 2) 
         time_fps = "Time: {}s, fps: {}".format(round(end - start, 2), round(1 / (end - start), 2))            
@@ -82,6 +152,21 @@ def main():
         if args.out_vid:
             out_vid.write(im)
         frameID+=1
+
+        # if not os.path.exists('saved_frames'):
+        #     os.makedirs('saved_frames')
+
+        # inp_vid = cv2.VideoCapture(args.input)
+        # num_frames = int(inp_vid.get(cv2.CAP_PROP_FRAME_COUNT))
+        
+        # for frameID in tqdm(range(num_frames)):
+        #     ret, im = inp_vid.read()
+
+        #     # ... (Your existing code for processing frames)
+
+        #     # Save frame to the new folder
+        #     frame_save_path = os.path.join('saved_frames', f'frame_{frameID + 1}.jpg')
+        #     cv2.imwrite(frame_save_path, im)
 
 def get_parser():
     parser = argparse.ArgumentParser(description="Yolov5 to (Deep)SORT demo")
@@ -103,7 +188,7 @@ def get_parser():
     parser.add_argument(
         "--max_dist",
         type=float, 
-        default=0.3,
+        default=0.5,
         help="Max cosine distance",
     )
     parser.add_argument("--nms_max_overlap",
@@ -115,7 +200,7 @@ def get_parser():
     parser.add_argument(
         "--conf-thres",
         type=float,
-        default=0.5,
+        default=0.3,
         help="Minimum score for instance predictions to be shown",
     )
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
@@ -125,7 +210,7 @@ def get_parser():
     parser.add_argument(
         "--display",
         type=bool,
-        default=False,
+        default=True,
         help="Streaming frames to display",
     )
     parser.add_argument(
@@ -143,7 +228,7 @@ def get_parser():
     parser.add_argument(
         "--out_txt",
         type=str,
-        default="output_txt.txt",
+        default="output_pose.txt",
         help="Write tracking results in MOT16 format to file seqtxt2write. To evaluate using pymotmetrics",
     )
     parser.add_argument(
